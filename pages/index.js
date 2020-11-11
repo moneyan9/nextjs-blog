f6f2f001a70601161c0a39badcc1c6806793c587
moneyan9 <m.tanimura1226@gmail.com>
2020-11-11 18:36:04 +0900

    for test vercel pull reauest

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I’m moneyan... I’m a software engineer and a translator (English/Japanese). You can contact me on <a href="https://twitter.com/moneyan9">Twitter</a>.</p>
+        <p>Hello, I’m moneyan...... I’m a software engineer and a translator (English/Japanese). You can contact me on <a href="https://twitter.com/moneyan9">Twitter</a>.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
